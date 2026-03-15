de67ccf89febf78bda5eda5b81da6c3c8454cdf0
NOMO OFC OWNER <mrdevile246@gmail.com>
2026-03-15 21:07:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://img.sanishtech.com/u/290d30b8f2d7b56c7ac9a025f2820b38.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 THARUWA-MD Is Alive Now😍*",
 BOT_OWNER: '94776121326',  // Replace with the owner's phone number
 
 
